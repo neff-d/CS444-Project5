ab582eb35ebc877e368ee83d6128d31fb598f198
Daniel Neff <neffd@oregonstate.edu>
2023-06-12 19:21:11 -0700

    June 12th

--- directory.c
+++ directory.c
@@ -6,6 +6,7 @@
 #include "block.h"
 #include "pack.h"
 #include "dirbasename.h"
+#include "mkfs.h"
 
 
 struct directory *directory_open(int inode_num) {
@@ -69,7 +70,55 @@ struct inode *namei(char *path) {
 
 int directory_make(char *path) {
 
+    char base_path[BLOCK_SIZE];
 
+    get_basename(path, base_path);
+
+    char dir_name[BLOCK_SIZE];
+    
+    get_dirname(path, dir_name);
+
+    struct inode *parent = namei(dir_name);
+    struct inode *in = ialloc();
+
+    if(in == NULL)
+        return -1;
+
+    int data_block_num = alloc();
+
+    if(data_block_num == -1) {
+        iput(in);
+        return -1;
+    }
+
+    char block[BLOCK_SIZE];
+
+    write_u16(block, in -> inode_num);
+    write_u16(block + ENTRY_SIZE, parent -> inode_num);
+
+    strcpy((char *) block + FILE_NAME_OFFSET, ".");
+    strcpy((char *) block + ENTRY_SIZE + FILE_NAME_OFFSET, "..");
+
+    in -> size = ENTRY_SIZE * DIR_BASE_ENTRIES;
+    in -> flags = DIR_FLAG_NUM;
+    in -> block_ptr[0] = data_block_num;
+
+    bwrite(data_block_num, block);
+
+    int block_index = parent -> size / BLOCK_SIZE;
+    int block_num = parent -> block_ptr[block_index];
+
+    bread(block_num, block);
+
+    write_u16(block + parent -> size, in -> inode_num);
+    strcpy(block + parent -> size + FILE_NAME_OFFSET, base_path);
+
+    parent -> size += ENTRY_SIZE;
+
+    bwrite(block_num, block);
+
+    iput(in);
+    iput(parent);
 
     return 0;
 }
